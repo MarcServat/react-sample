0db4f5b431efc20a877dd23e8d67cdb8d03c0dee
MarcServat <marcservat@hotmail.com>
2019-01-29 21:05:58 +0100

    WIP

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {renderToString} from 'react-dom/server';
 import {StaticRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import Routes from '../client/Routes';
 
-export default (req) => {
+export default (req, store) => {
     const content = renderToString(
-        <StaticRouter location={req.path} context={{}}>
-            <Routes></Routes>
-        </StaticRouter>);
+        <Provider store={store}>
+          <StaticRouter location={req.path} context={{}}>
+              <Routes/>
+          </StaticRouter>
+        </Provider>);
     return `
         <html lang="en">
             <head>
@@ -17,6 +20,5 @@ export default (req) => {
                 <div id="root">${content}</div>
                 <script src="bundle.js"></script>
             </body>
-        </html>
-    `;
+        </html>`;
 }
